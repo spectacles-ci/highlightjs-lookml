daf749b45f22606f063c487e3ea71e714e4c1729
Josh Temple <8672171+joshtemple@users.noreply.github.com>
2022-09-06 13:15:16 -0400

    Initial commit

---
+++ src/lookml.js
@@ -0,0 +1,11 @@
+/*
+Language: LookML
+Author: Josh Temple <josh@spectacles.dev>
+Description: Language definition for LookML (Looker's dependency language).
+*/
+
+export default function (hljs) {
+    return {
+        name: "LookML",
+    };
+}
