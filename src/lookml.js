90a54913c0d15e040ecb6e308ec17dd44accd44b
Josh Temple <8672171+joshtemple@users.noreply.github.com>
2023-04-27 16:53:21 -0400

    Fix tag highlighting in backticks

--- src/lookml.js
+++ src/lookml.js
@@ -16,7 +16,7 @@ export default function (hljs) {
     };
     const BLOCK_MODE = {
         scope: "block",
-        begin: [/\w+\s*:\s*/, /\+?/, /(\w+\s*)?/, /\{(?![%\{])/],
+        begin: [/\w+\s*:\s*/, /\+?/, /(\w+\s*)?/, /\{(?![%\{])`/],
         beginScope: {
             1: "keyword",
             2: "operator",
